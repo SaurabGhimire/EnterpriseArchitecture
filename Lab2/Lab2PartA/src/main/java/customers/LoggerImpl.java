d4bcbbf563fd88e1ea106e400072471cf4cbc3b2
Saurab Ghimire <sourrab@gmail.com>
2024-05-29 17:24:58 -0500

    remove test

--- Lab2/Lab2PartA/src/main/java/customers/LoggerImpl.java
+++ Lab2/Lab2PartA/src/main/java/customers/LoggerImpl.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 public class LoggerImpl implements Logger{
 
 	public void log(String logstring) {
-		System.out.println("Logging "+LocalDateTime.now()+" "+logstring);		
+		System.out.println("Logging "+LocalDateTime.now()+" "+logstring);
 	}
 
 }
