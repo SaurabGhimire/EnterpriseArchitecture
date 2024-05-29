2f194f4af5dc792cd99147a300672bb7142769ff
Saurab Ghimire <sourrab@gmail.com>
2024-05-29 15:16:50 -0500

    labd remove appconfig

--- Lab1/Lab1PartD/src/main/java/customers/AppConfig.java
+++ Lab1/Lab1PartD/src/main/java/customers/AppConfig.java
@@ -1,30 +1,32 @@
 package customers;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class AppConfig {
-    @Bean
-    public CustomerService customerService(){
-        CustomerService customerService = new CustomerServiceImpl();
-        customerService.setCustomerRepository(customerRepository());
-        customerService.setEmailSender(emailSender());
-        return customerService;
-    }
-
-    @Bean
-    public CustomerRepository customerRepository(){
-        return new CustomerRepositoryImpl(logger());
-    }
-
-    @Bean
-    public EmailSender emailSender(){
-        return new EmailSenderImpl(logger());
-    }
-
-    @Bean
-    public Logger logger(){
-        return new LoggerImpl();
-    }
+//    @Bean
+//    public CustomerService customerService(){
+//        CustomerService customerService = new CustomerServiceImpl();
+//        customerService.setCustomerRepository(customerRepository());
+//        customerService.setEmailSender(emailSender());
+//        return customerService;
+//    }
+//
+//    @Bean
+//    public CustomerRepository customerRepository(){
+//        return new CustomerRepositoryImpl(logger());
+//    }
+//
+//    @Bean
+//    public EmailSender emailSender(){
+//        return new EmailSenderImpl(logger());
+//    }
+//
+//    @Bean
+//    public Logger logger(){
+//        return new LoggerImpl();
+//    }
 }
