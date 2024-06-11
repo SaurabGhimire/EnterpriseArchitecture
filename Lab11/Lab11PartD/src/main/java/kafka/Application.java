cb7b2a2d8000977d3140f159099a3836e96e8644
Saurab Ghimire <sourrab@gmail.com>
2024-06-10 23:10:25 -0500

    send messages to create, deposit and withdraw using kafka

--- Lab11/Lab11PartD/src/main/java/kafka/Application.java
+++ Lab11/Lab11PartD/src/main/java/kafka/Application.java
@@ -19,8 +19,14 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        sender.send("createAccount", "Account 1");
-        System.out.println("Message has been sent");
+        sender.send("createAccount", "1, Account 1");
+        System.out.println("Create new account: Account 1");
+
+        sender.send("depositMoney", "1, 1000");
+        System.out.println("Deposit 1000 in account 1");
+
+        sender.send("withdrawMoney", "1, 100");
+        System.out.println("Withdraw 100 from account 1");
     }
 
 }
