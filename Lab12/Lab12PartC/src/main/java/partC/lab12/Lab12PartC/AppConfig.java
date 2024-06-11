1c4dda76e1e5bcaceb7fd7ac4527026a1fe35c99
Saurab Ghimire <sourrab@gmail.com>
2024-06-11 15:51:39 -0500

    added validation before reading config file

--- Lab12/Lab12PartC/src/main/java/partC/lab12/Lab12PartC/AppConfig.java
+++ Lab12/Lab12PartC/src/main/java/partC/lab12/Lab12PartC/AppConfig.java
@@ -4,19 +4,32 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "app.config")
+@Validated
 public class AppConfig {
+    @NotBlank
     String applicationName;
+    @NotBlank
     String version;
+    @NotBlank
+
     String serverUrl;
     String userFirstName;
     String userLastName;
+    @NotBlank
+    @Size(min = 8, max = 15)
     String userUserName;
+    @NotBlank
+    @Size(min = 8, max = 15)
+    String userPassword;
     List<String> countries;
 
     AppConfig(){
@@ -34,6 +47,7 @@ public class AppConfig {
                 ", userFirstName='" + userFirstName + '\'' +
                 ", userLastName='" + userLastName + '\'' +
                 ", userUserName='" + userUserName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
                 ", countries=" + countries +
                 '}';
     }
--- Lab12/Lab12PartC/src/main/java/partC/lab12/Lab12PartC/AppConfigService.java
+++ Lab12/Lab12PartC/src/main/java/partC/lab12/Lab12PartC/AppConfigService.java
@@ -2,11 +2,19 @@ package partC.lab12.Lab12PartC;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
-@Component
+
+@Service
+@Validated
 public class AppConfigService {
+    private final AppConfig appConfig;
+
     @Autowired
-    AppConfig appConfig;
+    public AppConfigService(AppConfig appConfig) {
+        this.appConfig = appConfig;
+    }
 
     public void print(){
         System.out.println(appConfig);
