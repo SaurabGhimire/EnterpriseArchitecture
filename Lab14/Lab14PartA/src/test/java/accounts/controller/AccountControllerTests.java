083d2fc0e2f6fa934f2011d63317d56f5cee64db
Saurab Ghimire <sourrab@gmail.com>
2024-06-12 15:53:33 -0500

    test for controller

--- Lab14/Lab14PartA/src/test/java/accounts/controller/AccountControllerTests.java
+++ Lab14/Lab14PartA/src/test/java/accounts/controller/AccountControllerTests.java
@@ -1,4 +1,45 @@
 package accounts.controller;
 
+import accounts.service.AccountResponse;
+import accounts.service.AccountService;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AccountController.class)
 public class AccountControllerTests {
+    @Autowired
+    MockMvc mock;
+
+    @MockBean
+    AccountService accountService;
+
+    @Test
+    public void testGetAccountByAccountNumber() throws  Exception {
+        String accountNumber = "1";
+        Mockito.when(accountService.getAccount(accountNumber)).thenReturn(
+                new AccountResponse(accountNumber, 100.0, "Account Holder 1")
+        );
+
+        mock.perform(MockMvcRequestBuilders.get("/account/"+accountNumber))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber").value(accountNumber))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountHolder").value("Account Holder 1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.balance").value(100.0));
+    }
 }
